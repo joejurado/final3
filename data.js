672544664866f659e03091d56cce2679aef0cbdd
joejurado <41087681+joejurado@users.noreply.github.com>
2018-12-10 05:47:00 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+var imps = {
+"type": "FeatureCollection",
+"name": "data",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "State_A": "Austria", "State_B": "Germany", "Imports": 2.85, "imp_LON": 14.13999, "imp_LAT": 47.59296, "exp_LON": 9.851, "exp_LAT": 51.11 }, "geometry": { "type": "Point", "coordinates": [ 14.13999, 47.59296 ] } },
+{ "type": "Feature", "properties": { "State_A": "Austria", "State_B": "Czech Republic", "Imports": 0.61, "imp_LON": 14.13999, "imp_LAT": 47.59296, "exp_LON": 15.338, "exp_LAT": 49.743 }, "geometry": { "type": "Point", "coordinates": [ 14.13999, 47.59296 ] } },
+{ "type": "Feature", "properties": { "State_A": "China", "State_B": "United States", "Imports": 7.54, "imp_LON": 103.83469, "imp_LAT": 36.56334, "exp_LON": -98.606, "exp_LAT": 39.622 }, "geometry": { "type": "Point", "coordinates": [ 103.83469, 36.56334 ] } },
+{ "type": "Feature", "properties": { "State_A": "China", "State_B": "Canada", "Imports": 4.53, "imp_LON": 103.83469, "imp_LAT": 36.56334, "exp_LON": -109.433, "exp_LAT": 59.081 }, "geometry": { "type": "Point", "coordinates": [ 103.83469, 36.56334 ] } },
+{ "type": "Feature", "properties": { "State_A": "France", "State_B": "Germany", "Imports": 3.48, "imp_LON": 2.55042, "imp_LAT": 46.56488, "exp_LON": 9.851, "exp_LAT": 51.11 }, "geometry": { "type": "Point", "coordinates": [ 2.55042, 46.56488 ] } },
+{ "type": "Feature", "properties": { "State_A": "France", "State_B": "Belgium", "Imports": 1.62, "imp_LON": 2.55042, "imp_LAT": 46.56488, "exp_LON": 4.664, "exp_LAT": 50.643 }, "geometry": { "type": "Point", "coordinates": [ 2.55042, 46.56488 ] } },
+{ "type": "Feature", "properties": { "State_A": "Germany", "State_B": "Poland", "Imports": 3.17, "imp_LON": 10.39233, "imp_LAT": 51.11015, "exp_LON": 19.401, "exp_LAT": 52.125 }, "geometry": { "type": "Point", "coordinates": [ 10.39233, 51.11015 ] } },
+{ "type": "Feature", "properties": { "State_A": "Germany", "State_B": "Sweden", "Imports": 2.85, "imp_LON": 10.39233, "imp_LAT": 51.11015, "exp_LON": 15.27, "exp_LAT": 62.011 }, "geometry": { "type": "Point", "coordinates": [ 10.39233, 51.11015 ] } },
+{ "type": "Feature", "properties": { "State_A": "Italy", "State_B": "Germany", "Imports": 1.82, "imp_LON": 12.07167, "imp_LAT": 42.79116, "exp_LON": 9.851, "exp_LAT": 51.11 }, "geometry": { "type": "Point", "coordinates": [ 12.07167, 42.79116 ] } },
+{ "type": "Feature", "properties": { "State_A": "Italy", "State_B": "Austria", "Imports": 1.51, "imp_LON": 12.07167, "imp_LAT": 42.79116, "exp_LON": 14.912, "exp_LAT": 47.683 }, "geometry": { "type": "Point", "coordinates": [ 12.07167, 42.79116 ] } },
+{ "type": "Feature", "properties": { "State_A": "Japan", "State_B": "China", "Imports": 3.17, "imp_LON": 137.97387, "imp_LAT": 37.53982, "exp_LON": 106.514, "exp_LAT": 33.42 }, "geometry": { "type": "Point", "coordinates": [ 137.97387, 37.53982 ] } },
+{ "type": "Feature", "properties": { "State_A": "Japan", "State_B": "United States", "Imports": 2.16, "imp_LON": 137.97387, "imp_LAT": 37.53982, "exp_LON": -98.606, "exp_LAT": 39.622 }, "geometry": { "type": "Point", "coordinates": [ 137.97387, 37.53982 ] } },
+{ "type": "Feature", "properties": { "State_A": "Netherlands", "State_B": "Germany", "Imports": 2.78, "imp_LON": 5.601, "imp_LAT": 52.25358, "exp_LON": 9.851, "exp_LAT": 51.11 }, "geometry": { "type": "Point", "coordinates": [ 5.601, 52.25358 ] } },
+{ "type": "Feature", "properties": { "State_A": "Netherlands", "State_B": "Belgium", "Imports": 1.55, "imp_LON": 5.601, "imp_LAT": 52.25358, "exp_LON": 4.664, "exp_LAT": 50.643 }, "geometry": { "type": "Point", "coordinates": [ 5.601, 52.25358 ] } },
+{ "type": "Feature", "properties": { "State_A": "Korea, Republic of", "State_B": "China", "Imports": 1.26, "imp_LON": 127.82934, "imp_LAT": 36.3677, "exp_LON": 106.514, "exp_LAT": 33.42 }, "geometry": { "type": "Point", "coordinates": [ 127.82934, 36.3677 ] } },
+{ "type": "Feature", "properties": { "State_A": "Korea, Republic of", "State_B": "United States", "Imports": 0.87, "imp_LON": 127.82934, "imp_LAT": 36.3677, "exp_LON": -98.606, "exp_LAT": 39.622 }, "geometry": { "type": "Point", "coordinates": [ 127.82934, 36.3677 ] } },
+{ "type": "Feature", "properties": { "State_A": "United Kingdom", "State_B": "Germany", "Imports": 2.34, "imp_LON": -2.9005, "imp_LAT": 54.16018, "exp_LON": 9.851, "exp_LAT": 51.11 }, "geometry": { "type": "Point", "coordinates": [ -2.9005, 54.16018 ] } },
+{ "type": "Feature", "properties": { "State_A": "United Kingdom", "State_B": "China", "Imports": 2.27, "imp_LON": -2.9005, "imp_LAT": 54.16018, "exp_LON": 106.514, "exp_LAT": 33.42 }, "geometry": { "type": "Point", "coordinates": [ -2.9005, 54.16018 ] } },
+{ "type": "Feature", "properties": { "State_A": "United States", "State_B": "Canada", "Imports": 18.71, "imp_LON": -112.49355, "imp_LAT": 45.69548, "exp_LON": -109.433, "exp_LAT": 59.081 }, "geometry": { "type": "Point", "coordinates": [ -112.49355, 45.69548 ] } },
+{ "type": "Feature", "properties": { "State_A": "United States", "State_B": "China", "Imports": 10.36, "imp_LON": -112.49355, "imp_LAT": 45.69548, "exp_LON": 106.514, "exp_LAT": 33.42 }, "geometry": { "type": "Point", "coordinates": [ -112.49355, 45.69548 ] } }
+]
+}
